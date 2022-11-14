adf87cc33cab9b6fdad5a8db90d78a3040243537
luis.map <luismap2508@gmail.com>
2022-11-14 12:23:27 +0100

    [refac] updating to use infra pacakage

--- src/main/scala/features/userpayment/presentation/SimpleProducer.scala
+++ src/main/scala/features/userpayment/presentation/SimpleProducer.scala
@@ -1,28 +1,41 @@
 package features.userpayment.presentation
 
+import core.infra.topics.MyKafkaProducer
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.log4j.Logger
 
 import java.util.Properties
 import scala.io.Source
 
 object SimpleProducer extends App {
 
+  val logger = Logger.getLogger(this.getClass.getName)
   val props = new Properties()
   props.load(this.getClass.getResourceAsStream("/secret_kafka_cluster.properties"))
   props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
   props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
   props.put("group.id", "simple_producer01")
 
-  val producer = new KafkaProducer[String, String](props)
+  val producer =  MyKafkaProducer.create[String, String](props)
+  var keepRunning = true
+  val mainThread = Thread.currentThread()
 
-  while (true) {
-    val content = Source.stdin.getLines()
-    for (
-      line <- content
-    ) {
-      val data = line.split(":")
-      producer.send(new ProducerRecord[String, String]("poems", data(0), data(1)))
-    }
-    producer.commitTransaction()
+  Runtime.getRuntime().addShutdownHook(new Thread(() => {
+    keepRunning = false
+    logger.info("got into new thread")
+    mainThread.join()
+  }))
+
+  while (keepRunning) {
+      val content = Source.stdin.getLines()
+      for (
+        line <- content
+      ) {
+        val data = line.split(":")
+        producer.send(new ProducerRecord[String, String]("poems", data(0), data(1)))
+      }
+      producer.commitTransaction()
+
   }
+
 }
